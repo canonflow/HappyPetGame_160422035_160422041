2ff3794a0bbab321e89b02a5071b0d3fc036ecaa
Nathan Garzya Santoso <nathangarzyasantoso7@gmail.com>
2023-07-05 23:25:34 +0700

    Class Player sementara gini dulu

--- HappyPetGame_160422035_160422041/Pet.cs
+++ HappyPetGame_160422035_160422041/Pet.cs
@@ -1,11 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 
 namespace HappyPetGame_160422035_160422041
 {
-    public class Pet
+    public abstract class Pet
     {
+        #region FIELDS
+        private string name;
+        private string health;
+        private string energy;
+        private string happiness;
+        private Image picture;
+        private Player player;
+        private List<Toy> listOfToys;
+
+        //! For PET DEFENDER MODE
+        private int healthPoint;
+        private int levelAttack1;
+        private int damageAttack1;
+        private int levelAttack2;
+        private int damageAttack2;
+        private int levelAttack3;
+        private int damageAttack3;
+        #endregion
+
+        #region CONSTRUCTOR
+
+        #endregion
+
+        #region PROPERTIES
+
+        #endregion
+
+        #region METHODS
+
+        #endregion
     }
 }
--- HappyPetGame_160422035_160422041/Player.cs
+++ HappyPetGame_160422035_160422041/Player.cs
@@ -7,5 +7,85 @@ namespace HappyPetGame_160422035_160422041
 {
     public class Player
     {
+        #region FIELDS
+        private string username;  // !Di input waktu sign in / sign up <UNIQUE>
+        private DateTime lastPlay;
+        private int coins;
+
+        //! For PET DEFENDER MODE
+        private List<Player> listOfFriends;
+        private List<Player> listOfRequested;
+        private int petDefenderPoint;
+        #endregion
+
+        #region CONSTRUCTOR
+        public Player(string username)
+        {
+            this.Username = username;
+            this.lastPlay = DateTime.Now;
+            this.Coins = 100;
+            this.listOfFriends = new List<Player>();
+            this.ListOfRequested = new List<Player>();
+            this.PetDefenderPoint = 0;
+            
+        }
+        #endregion
+
+        #region PROPERTIES
+        public string Username 
+        { 
+            get => username; 
+            set
+            {
+                if (value != "")
+                {
+                    username = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Player username can't be EMPTY!!");
+                }
+            } 
+        }
+        public DateTime LastPlay { get => lastPlay; set => lastPlay = value; }
+        public int Coins { get => coins; set => coins = value; }
+        public List<Player> ListOfFriends { get => listOfFriends; set => listOfFriends = value; }
+        public List<Player> ListOfRequested { get => listOfRequested; set => listOfRequested = value; }
+        public int PetDefenderPoint { get => petDefenderPoint; set => petDefenderPoint = value; }
+        #endregion
+
+        #region METHODS
+        public string DisplayData()
+        {
+            string data = "Username: " + this.Username +
+                          "\nCoins: " + this.Coins;
+
+            return data;
+        }
+
+        public void AcceptFriendRequest(Player player)
+        {
+            ListOfFriends.Add(player);
+
+            foreach (Player requested in ListOfRequested )
+            {
+                if (requested.Username == player.Username)
+                {
+                    ListOfRequested.Remove(player);
+                }
+            }
+        }
+
+        public void RemoveFriend(Player player)
+        {
+            foreach (Player friend in ListOfFriends)
+            {
+                if (friend.Username == player.Username)
+                {
+                    ListOfFriends.Remove(player);
+                }
+            }
+        }
+        #endregion
     }
 }
