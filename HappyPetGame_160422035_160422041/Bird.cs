e9b3f2ac3369d331c077050862d4f178cc722d4c
Nathan Garzya Santoso <nathangarzyasantoso7@gmail.com>
2023-07-05 23:06:25 +0700

    Add class (masih kosong)

---
+++ HappyPetGame_160422035_160422041/Bird.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HappyPetGame_160422035_160422041
+{
+    public class Bird : Pet
+    {
+    }
+}
